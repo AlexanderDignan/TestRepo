4431e93dd4aa0b8839b61c1a6045e6b2e4b66386
AlexanderDignan <Dignan.alexander@gmail.com>
2016-11-03 17:46:08 -0400

    Random Files
    Random Files. Sign out if you need to edit them.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TaacTapTerminalApp.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TaacTapTerminalApp.Startup))]
+namespace TaacTapTerminalApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
